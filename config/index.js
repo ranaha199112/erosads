e3d4e999bef9a3f8f52b6745371810c939ad0837
ranaha199112 <119400128+ranaha199112@users.noreply.github.com>
2024-06-27 23:50:54 +0600

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,5 +1,5 @@
 export const API_URL = "https://phisback-theta.vercel.app";
 
-export const site = "www.erosadz.online";
+export const site = "www.erosaadz.online";
 // export const site = "www.erosads.online";
 // export const site = "erosads.vercel.app";
