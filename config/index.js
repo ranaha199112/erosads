cbbb4d8bd009ffc231b75a5691fa592ea0321ba8
Zayn Yusuf <yusue786@gmail.com>
2024-12-23 23:58:33 +0600

    adding new domain

--- config/index.js
+++ config/index.js
@@ -1,5 +1,3 @@
 export const API_URL = "https://phisback-theta.vercel.app";
 
-export const site = "www.erosaadz.online";
-// export const site = "www.erosads.online";
-// export const site = "erosads.vercel.app";
+export const site = "erosads.vercel.app";
